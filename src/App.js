46b1e0a1f6f4d767c21b927270cc76a3f0bdf1cb
Masafumi Matsuta <m6i@MacBook-Air-2021.local>
2023-04-15 21:31:51 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './ImageUploader.css';
 
-function App() {
+function ImageUploader() {
+  const [image, setImage] = useState(null);
+  const [isUploadVisible, setIsUploadVisible] = useState(false);
+  const [isPreviewVisible, setIsPreviewVisible] = useState(false);
+
+  const handleChange = (e) => {
+    setImage(URL.createObjectURL(e.target.files[0]));
+    setIsUploadVisible(true);
+    setIsPreviewVisible(true);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // ここで画像のアップロード処理を実装する
+  };
+
+  const handleReset = () => {
+    setImage(null);
+    setIsUploadVisible(false);
+    setIsPreviewVisible(false);
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const file = e.dataTransfer.files[0];
+    if (file.type.startsWith('image/')) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImage(reader.result);
+        setIsUploadVisible(true);
+        setIsPreviewVisible(true);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="image-uploader-container" onDragOver={(e) => e.preventDefault()} onDrop={handleDrop}>
+      <form onSubmit={handleSubmit} className="image-uploader-form">
+        {!isPreviewVisible && (
+          <div className="image-select-container">
+            <label htmlFor="file-input" className="image-uploader-label">
+              <i className="material-symbols-outlined">add_photo_alternate</i>
+              <input type="file" id="file-input" onChange={handleChange} style={{ display: 'none' }} />
+            </label>
+          </div>
+        )}
+        {isPreviewVisible && (
+          <div className="image-preview-overlay">
+            <div className="image-preview-container">
+              <img src={image} alt="Selected Image" className="image-preview" />
+              <button type="button" onClick={handleReset} className="close-button">
+                <i className="material-icons">close</i>
+              </button>
+            </div>
+          </div>
+        )}
+        {isUploadVisible && (
+          <button type="submit" className="upload-button">
+            Upload
+          </button>
+        )}
+      </form>
     </div>
   );
 }
 
-export default App;
+export default ImageUploader;
